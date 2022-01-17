15210728ebab68a404517768bc1bb223fa5931d7
Mouez98 <mouez.matoui@gmail.com>
2022-01-17 12:14:03 +0100

    Tried Context API

---
+++ src/context/auth-context.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from "react";
+
+const AuthContext = React.createContext({
+    isLoggedIn: false,
+    onLogut: ()=> {},
+    onLogin: (email, password) => {}
+})
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    
+    useEffect(() => {
+        if (localStorage.getItem("loggedIn") === "1") {
+          setIsLoggedIn(true);
+        }
+      }, []);
+
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        setIsLoggedIn(true);
+    
+        localStorage.setItem("loggedIn", "1");
+      };
+
+    const logoutHandler = () => {
+        localStorage.removeItem("loggedIn");
+        setIsLoggedIn(false);
+      };
+   return <AuthContext.Provider value={{
+       isLoggedIn: isLoggedIn,
+       onLogout: logoutHandler,
+       onLogin: loginHandler
+   }}>
+       {props.children}
+   </AuthContext.Provider>
+}
+
+export default AuthContext;
